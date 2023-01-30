efdd055de822216fb35696cd286d29f278c90170
Ismael Shakverdiev <me@theiskaa.com>
2023-01-30 22:30:18 +0400

    chore: clean up tests

--- test/hidable_test.dart
+++
@@ -1,9 +0,0 @@
-//
-// Copyright 2021-2022 present Insolite. All rights reserved.
-// Use of this source code is governed by Apache 2.0 license
-// that can be found in the LICENSE file.
-//
-
-void main() {
-    // Test can be found in test/src/ ...
-}
